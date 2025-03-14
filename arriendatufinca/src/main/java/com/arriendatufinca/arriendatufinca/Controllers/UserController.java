98cf682889f157718a4775ac1428a9103224f5e6
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-14 13:43:41 -0500

    Update: Se inicio la creación de controlador de usuario

--- arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Controllers/UserController.java
+++ arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Controllers/UserController.java
@@ -6,20 +6,36 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arriendatufinca.arriendatufinca.Repositories.UserRepository;
+import com.arriendatufinca.arriendatufinca.Entities.User;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 
 
 @RestController
-@RequestMapping(value = "/api/users")
+@RequestMapping("/api/user")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
 
     @CrossOrigin
-    @GetMapping("path")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    @GetMapping("/{id}")
+    public User get(@PathVariable Long id) throws Exception {
+        return userRepository.findById(id).orElseThrow(() -> new Exception("User not found"));
     }
+
+    @CrossOrigin
+    @GetMapping("/all")
+    public Iterable<User> getAll() {
+        return userRepository.findAll();
+    }
+
+    @CrossOrigin
+    @GetMapping("/delete/{id}")
+    public void delete(@PathVariable Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @CrossOrigin
+    @GetMapping("/create/{username}/{name}/{lasname}/{email}")
     
 }
