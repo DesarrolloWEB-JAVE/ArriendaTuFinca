699439b47cc73c6259eb309ee0fb3ad5ce5f61ff
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-14 22:47:26 -0500

    Feature: Se implementó la conexión a la base de datos remota

--- arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Conections/SshTunnelStarter.java
+++ arriendatufinca/src/main/java/com/arriendatufinca/arriendatufinca/Conections/SshTunnelStarter.java
@@ -1,48 +1,6 @@
-/* package com.arriendatufinca.arriendatufinca.Conections;
-
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.Session;
-
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
-
-public class SshTunnelStarter {
-
-    private String url = "10.43.96.52";
-
-    private String username = "estudiante";
-
-    private String password = "1Rioblanco/7";
-
-    private int port = 22;
-
-    private Session session;
-
-    @PostConstruct
-    public void init() throws Exception {
-       JSch jsch = new JSch();
-       // Get SSH session
-       session = jsch.getSession(username, url, port);
-       session.setPassword(password);
-       java.util.Properties config = new java.util.Properties();
-       // Never automatically add new host keys to the host file
-       config.put("StrictHostKeyChecking", "no");
-       session.setConfig(config);
-       // Connect to remote server
-       session.connect();
-       // Apply the port forwarding
-       session.setPortForwardingL(3306, url, 3306);
-    }
-
-    @PreDestroy
-    public void shutdown() throws Exception {
-       if (session != null && session.isConnected()) {
-           session.disconnect();
-       }
-    }
-}
-    */
 package com.arriendatufinca.arriendatufinca.Conections;
+
+import java.util.ResourceBundle;
 
 import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.JSchException;
@@ -53,13 +11,14 @@ public class SshTunnelStarter {
     private Session session;
 
     public void init() throws JSchException {
-        String sshHost = "10.43.96.52";
-        String sshUser = "estudiante";
-        String sshPassword = "1Rioblanco/7";
-        int sshPort = 22;
-        int localPort = 3307; // Change this to a different port
-        String remoteHost = "localhost";
-        int remotePort = 3306;
+        ResourceBundle bundle = ResourceBundle.getBundle("application");
+        String sshHost = bundle.getString("spring.ssh.host");
+        String sshUser = bundle.getString("spring.ssh.user");
+        String sshPassword = bundle.getString("spring.ssh.password");
+        int sshPort = Integer.parseInt(bundle.getString("spring.ssh.port"));
+        String remoteHost = bundle.getString("spring.ssh.remoteHost");
+        int remotePort = Integer.parseInt(bundle.getString("spring.ssh.remotePort"));
+        int localPort = Integer.parseInt(bundle.getString("spring.ssh.localPort"));
 
         JSch jsch = new JSch();
         session = jsch.getSession(sshUser, sshHost, sshPort);
